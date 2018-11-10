4d1099e8a5c270e5b1582acf965d7e2cfc2ba4d6
Jonathan Freed <jon.freed@gmail.com>
2018-11-10 19:16:18 +0000

    automatically backed up by learn

--- conditionals.rb
+++ conditionals.rb
@@ -3,9 +3,9 @@ puts "Hello! What's your name?"
 input = gets.chomp
 
 def greet(name)
-if name == "Jon"
-  puts "Hello #{name}!"
-end
+  if name == "Jon"
+    puts "Hello #{name}!"
+  end
 end
 
 greet(input)
